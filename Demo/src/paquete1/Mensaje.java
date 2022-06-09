ef99b187400065c65b2ea22f72af4d027d203ed2
Isaac-Quezada <imquezada1@utpl.edu.ec>
2022-06-09 16:42:37 -0500

    Taller 00 Resubido

--- Demo/src/paquete1/Mensaje.java
+++ Demo/src/paquete1/Mensaje.java
@@ -1,5 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package paquete1;
 import paquete4.Palabra;
+/**
+ *
+ * @author reroes
+ */
 public class Mensaje {
 
     public static String obtenerFrase() {
--- Demo/src/paquete2/Demo.java
+++ Demo/src/paquete2/Demo.java
@@ -1,13 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package paquete2;
+
 import paquete1.Mensaje;
-import paquete5.Dato;
 import paquete3.Valor;
+/**
+ *
+ * @author reroes
+ */
 public class Demo {
-    
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         
-        // System.out.printf(Mensaje.obtenerFrase()+ " con " + (Dato.obtenerNumeroHombres()+ Dato.obtenerNumeroMujeres()));
-        System.out.printf("%s con %d %n", Mensaje.obtenerFrase(),Valor.obtenerHabitantes());
+        System.out.printf("%s con %d",Mensaje.obtenerFrase(),Valor.obtenerHabitantes());
     }
     
 }
